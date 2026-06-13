d6d6b84c83fdee4ccad5049beb1c2e0b52abb153
Andika Dinata <a.introvrt@gmail.com>
2026-06-14 03:17:50 +0700

    ngantuk anjay

--- worker/index.js
+++ worker/index.js
@@ -9,12 +9,16 @@ export default {
     }
 
     // Serve static assets
+    if (!env.ASSETS) {
+      return new Response("Assets binding not found. If this is local dev, check your configuration.", { status: 500 });
+    }
+
     const response = await env.ASSETS.fetch(request);
 
     // If the request is for index.html (or a SPA route that serves index.html),
     // inject the environment variables.
     const contentType = response.headers.get("content-type");
-    if (contentType && contentType.includes("text/html")) {
+    if (response.ok && contentType && contentType.includes("text/html")) {
       return new HTMLRewriter()
         .on("head", {
           element(element) {
@@ -23,7 +27,7 @@ export default {
               VITE_CRUD_SERVICE_URL: env.VITE_CRUD_SERVICE_URL,
             };
 
-            element.append(`<script>window.ENV = ${JSON.stringify(vars)};</script>`, { html: true });
+            element.prepend(`<script>window.ENV = ${JSON.stringify(vars)};</script>`, { html: true });
           },
         })
         .transform(response);
